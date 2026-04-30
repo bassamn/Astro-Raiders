6975c3f203676d0df525bbce774c5108c127f419
Bassam Nazemi <bassamna@proton.me>
2026-04-30 23:28:55 +0330

    Add boundaries to screen, Fix aspect ratio
    Add boundaries to screen to avoid exiting rocket from screen
    Aspect ratio was set to 10:16 by mistake (9:16 is correct)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,22 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float leftBoundPadding;
+    [SerializeField] float rightBoundPadding;
+    [SerializeField] float downBoundPadding;
+    [SerializeField] float topBoundPadding;
 
     InputAction moveAction;
 
     Vector3 moveVector;
+    Vector2 minBounds;
+    Vector2 maxBounds;
+
     void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");
+
+        InitBounds();
     }
 
     void Update()
@@ -18,10 +27,21 @@ public class PlayerController : MonoBehaviour
         MovePlayer();
     }
 
+    void InitBounds()
+    {
+        Camera mainCamera = Camera.main;
+        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
+        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
+    }
+
     void MovePlayer()
     {
         moveVector = moveAction.ReadValue<Vector2>();
+        Vector3 newPos = transform.position + moveVector * moveSpeed * Time.deltaTime;
 
-        transform.position += moveVector * moveSpeed * Time.deltaTime;
+        newPos.x = Math.Clamp(newPos.x, minBounds.x + leftBoundPadding, maxBounds.x - rightBoundPadding);
+        newPos.y = Math.Clamp(newPos.y, minBounds.y + downBoundPadding, maxBounds.y - topBoundPadding);
+
+        transform.position = newPos;
     }
 }
